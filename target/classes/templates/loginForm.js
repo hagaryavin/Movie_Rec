7c16aedba53de93993f47b83dfeaa4b551ca92cc
hagaryavin <72992212+hagaryavin@users.noreply.github.com>
2021-06-30 17:35:41 +0300

    commit

--- target/classes/templates/loginForm.js
+++ target/classes/templates/loginForm.js
@@ -10,24 +10,25 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(<loginForm />, rootElement);
 
 /*
+    <form action="#" th:action="@{/}" th:object="${User}" method="post">
+        <table>
+            <tr>
+                <td>Name:</td>
+                <td><input type="text" th:field="*{userName}" /></td>
+                <td th:if="${#fields.hasErrors('userName')}" th:errors="*{userName}"></td>
+            </tr>
+            <tr>
+                <td>Age:</td>
+                <td><input type="text" th:field="*{password}" /></td>
+                <td th:if="${#fields.hasErrors('password')}" th:errors="*{password}"></td>
+            </tr>
+            <tr>
+                <td><button type="submit">Login</button></td>
+            </tr>
+        </table>
+    </form>
 
-      <form action="#" th:action="@{/}" th:object="${User}" method="post">
-            <table>
-                <tr>
-                    <td>Name:</td>
-                    <td><input type="text" th:field="*{userName}" /></td>
-                    <td th:if="${#fields.hasErrors('userName')}" th:errors="*{userName}"></td>
-                </tr>
-                <tr>
-                    <td>Age:</td>
-                    <td><input type="text" th:field="*{password}" /></td>
-                    <td th:if="${#fields.hasErrors('password')}" th:errors="*{password}"></td>
-                </tr>
-                <tr>
-                    <td><button type="submit">Login</button></td>
-                </tr>
-            </table>
-        </form>
+
 
 
 
